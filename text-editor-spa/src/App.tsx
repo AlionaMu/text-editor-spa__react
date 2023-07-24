affe0de59a38e7d3b2a6a14d3fbed352062a382a
AlionaMu <alionazimaby@gmail.com>
2023-07-24 14:01:38 +0300

    feat: add components

--- text-editor-spa/src/App.tsx
+++ text-editor-spa/src/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CardsList } from "./components/CardsList/CardsList";
+import { Form } from "./components/Form/Form";
+import { Header } from "./components/Header/Header";
+import TagsList from "./components/TagsList/TagsList";
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import { ThemeContext } from "./context/theme-context";
+import { useSelector } from "react-redux";
+import { RootState } from "./store";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
+  const isBrowserDefaultDark = () =>
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const getDefaultTheme = (): string => {
+    const localStorageTheme = localStorage.getItem("default-theme");
+    const browserDefault = isBrowserDefaultDark() ? "dark" : "light";
+    return localStorageTheme || browserDefault;
+  };
+  const [theme, setTheme] = useState(getDefaultTheme());
+
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (language: string) => {
+    i18n.changeLanguage(language);
+  };
+
+  const [filter, setFilter] = useState("");
+
+  const state = useSelector((state: RootState) => state.notesList);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={`theme-${theme}`}>
+        <div className="App">
+          <Header changeLanguage={changeLanguage} t={t}></Header>
+          <main className="main">
+            <section className="container">
+              <Form t={t}></Form>
+              <TagsList
+                items={state.tagsAmount}
+                setFilter={setFilter}
+                t={t}
+              ></TagsList>
+            </section>
+            <CardsList filter={filter} list={state.notesList} t={t}></CardsList>
+          </main>
+          <Footer></Footer>
+        </div>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
